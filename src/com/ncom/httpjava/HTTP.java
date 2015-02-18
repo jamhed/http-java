73b8f03ca290cf0ca3bec2e8f96c91f48e36cef0
Roman Galeev <ip@ncom-ufa.ru>
2015-02-18 08:41:48 +0100

    indent

--- src/com/ncom/httpjava/HTTP.java
+++ src/com/ncom/httpjava/HTTP.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -114,7 +113,6 @@ class HTTP {
                 HttpPost req = new HttpPost(url);
 
                 String result = "";
-                
 				if (body.length() > 0) {
                     try {
 						req.setEntity(new StringEntity(body));
